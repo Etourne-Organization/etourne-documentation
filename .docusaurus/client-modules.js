978ce76118c7591b9aab299894458a77da76edf0
Muntasir2001 <muntasirenamalhuda@gmail.com>
2023-05-12 18:01:55 +0100

    working on doc

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/alhudam/Desktop/projects/etourne-documentation/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/alhudam/Desktop/projects/etourne-documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/alhudam/Desktop/projects/etourne-documentation/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/alhudam/Desktop/projects/etourne-documentation/src/css/custom.css'),
+];
